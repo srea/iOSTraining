e7f170951bea0135c72de266bb10eedc692f05cb
yuki tamazawa <yuki.tamazawa@gmo-media.jp>
2015-04-10 16:13:12 +0900

    1.3宿題おわりました

--- HomeWorks/1.3/NiceModalViewAnswer/NiceModalView/MixiAppDelegate.h
+++ HomeWorks/1.3/NiceModalViewAnswer/NiceModalView/MixiAppDelegate.h
@@ -13,6 +13,12 @@
 
 @interface MixiAppDelegate : UIResponder <UIApplicationDelegate>
 
+
+
+
+
+
+
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) MixiNiceViewController *viewController;
 @end
